95058362927f626708c6b731fc7a0e17313b8931
Sthabiraj <birajshrestha2007@gmail.com>
2024-07-19 10:28:18 +0545

    ✨ feat: Update prettier to version 3.3.3 and add prettier-plugin-tailwindcss

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -40,7 +40,7 @@ export default function LoginForm() {
           </Label>
           <div className="relative">
             <Mail
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+              className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400"
               size={18}
             />
             <Input
@@ -49,7 +49,7 @@ export default function LoginForm() {
               type="email"
               placeholder="Enter your email"
               required
-              className="pl-10 border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+              className="border-gray-300 pl-10 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
             />
           </div>
         </div>
@@ -62,7 +62,7 @@ export default function LoginForm() {
           </Label>
           <div className="relative">
             <Lock
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+              className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400"
               size={18}
             />
             <Input
@@ -71,13 +71,13 @@ export default function LoginForm() {
               type="password"
               placeholder="Enter your password"
               required
-              className="pl-10 border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+              className="border-gray-300 pl-10 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
             />
           </div>
         </div>
         <Button
           type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white"
+          className="w-full bg-blue-500 text-white hover:bg-blue-600"
         >
           Sign In
         </Button>
@@ -88,14 +88,14 @@ export default function LoginForm() {
           <span className="w-full border-t border-gray-300"></span>
         </div>
         <div className="relative flex justify-center text-sm">
-          <span className="px-2 bg-white text-gray-500">Or continue with</span>
+          <span className="bg-white px-2 text-gray-500">Or continue with</span>
         </div>
       </div>
 
       <Button
         type="submit"
         variant="outline"
-        className="w-full bg-white text-gray-800 hover:bg-gray-100 border border-gray-300"
+        className="w-full border border-gray-300 bg-white text-gray-800 hover:bg-gray-100"
         onClick={() => githubLogin()}
         disabled={isLoadingGithub}
       >
@@ -108,7 +108,7 @@ export default function LoginForm() {
           <>
             <svg
               viewBox="0 0 24 24"
-              className="h-5 w-5 mr-2"
+              className="mr-2 h-5 w-5"
               fill="currentColor"
             >
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
